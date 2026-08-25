0fdc89c117e2971293d85d5db0aebc11c2073b5b
demo43890 <demo43890@gmail.com>
2026-08-25 21:24:05 +0530

    Implement getUserProfile function in api.js

---
+++ api.js
@@ -0,0 +1,19 @@
+const { getUser } = require("./users");
+
+function getUserProfile(id) {
+  const user = getUser(id);
+
+  if (!user) {
+    return {
+      error: "User not found"
+    };
+  }
+
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email
+  };
+}
+
+module.exports = { getUserProfile };
