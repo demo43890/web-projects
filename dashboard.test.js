89681f1284cea9e42169e668707bc755c7202d7e
demo43890 <demo43890@gmail.com>
2026-08-25 21:24:31 +0530

    Add test for dashboard user count

---
+++ dashboard.test.js
@@ -0,0 +1,13 @@
+const { getDashboard } = require("./dashboard");
+
+function testDashboard() {
+  const dashboard = getDashboard();
+
+  if (dashboard.totalUsers !== 2) {
+    throw new Error("User count is incorrect");
+  }
+
+  console.log("Dashboard test passed");
+}
+
+testDashboard();
