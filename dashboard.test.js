2b8a3a865d1aaa102f0028d207e990cbf1fb0c87
demo43890 <demo43890@gmail.com>
2026-08-25 21:28:57 +0530

    Update dashboard test

--- dashboard.test.js
+++ dashboard.test.js
@@ -3,8 +3,7 @@ const { getDashboard } = require("./dashboard");
 function testDashboard() {
   const dashboard = getDashboard();
 
-  if (dashboard.totalUsers !== 2) {
-    throw new Error("User count is incorrect");
+if (dashboard.totalUsers !== 2 || dashboard.activeUsers !== 2) {  throw new Error("User count is incorrect");
   }
 
   console.log("Dashboard test passed");
